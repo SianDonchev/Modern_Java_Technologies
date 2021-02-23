a0ba6937504225d5d19769eb86e9da43f7afbfd1
unknown <siandonchev@gmail.com>
2021-02-23 18:30:21 +0200

    blocked urls logic added;logic of conversion of BGN - EUR added

--- Lab-2/Revolut/src/bg/sofia/uni/fmi/mjt/revolut/Revolut.java
+++ Lab-2/Revolut/src/bg/sofia/uni/fmi/mjt/revolut/Revolut.java
@@ -1,11 +1,17 @@
 package bg.sofia.uni.fmi.mjt.revolut;
+
 import bg.sofia.uni.fmi.mjt.revolut.account.Account;
 import bg.sofia.uni.fmi.mjt.revolut.card.Card;
+
 import java.time.LocalDate;
 
 public class Revolut implements RevolutAPI {
     private Account[] accounts;
     private Card[] cards;
+
+    public static final double EUR_TO_BGN_EXCHANGE_RATE = 1.95583;
+    public static final String BLOCKED_TOP_LEVEL_DOMAIN = "biz";
+
 
     Revolut(Account[] accounts, Card[] cards) {
         this.accounts = accounts;
@@ -14,29 +20,35 @@ public class Revolut implements RevolutAPI {
 
     @Override
     public boolean pay(Card card, int pin, double amount, String currency) {
-        if (card.getType() == "PHYSICAL" && isAccepted(card,pin,amount,currency)) {
+        if (card.getType() == "PHYSICAL" && isAccepted(card, pin, amount, currency)) {
             int indexForAccountToWithdraw = accountWithTheSufficientAmount(amount);
             Account accountToWithdrawFrom = accounts[indexForAccountToWithdraw];
-
             accountToWithdrawFrom.withdraw(amount);
+            if (card.getType().equals("VIRTUALONETIME")) {
+                card.block();
+            }
+            return true;
         }
         return false;
     }
 
     @Override
     public boolean payOnline(Card card, int pin, double amount, String currency, String shopURL) {
-        if (isAccepted(card,pin,amount,currency)) { // TO DO
+        if (!isBlockedURL(shopURL) && isAccepted(card, pin, amount, currency)) {
             int indexForAccountToWithdraw = accountWithTheSufficientAmount(amount);
             Account accountToWithdrawFrom = accounts[indexForAccountToWithdraw];
-
             accountToWithdrawFrom.withdraw(amount);
+            if (card.getType().equals("VIRTUALONETIME")) {
+                card.block();
+            }
+            return true;
         }
         return false;
     }
 
     @Override
     public boolean addMoney(Account account, double amount) {
-        if(isValidAccount(account)){
+        if (isValidAccount(account)) {
             account.addMoney(amount);
             return true;
         }
@@ -45,10 +57,17 @@ public class Revolut implements RevolutAPI {
 
     @Override
     public boolean transferMoney(Account from, Account to, double amount) {
-        if(isValidAccount(from) && isValidAccount(to) && !from.equals(to) && from.hasEnoughMoney(amount)){
+        if (isValidAccount(from) && isValidAccount(to) && !from.equals(to) && from.hasEnoughMoney(amount)) {
+
+            if (from.getCurrency().equals(to.getCurrency())) {
+                from.withdraw(amount);
+                to.addMoney(amount);
+                return true;
+            }
+
+            double convertedAmmount = from.getCurrency().equals("BGN") ? amount / EUR_TO_BGN_EXCHANGE_RATE : amount * EUR_TO_BGN_EXCHANGE_RATE;
             from.withdraw(amount);
-            to.addMoney(amount);
-            return true;
+            to.addMoney(convertedAmmount);
         }
         return false;
     }
@@ -56,15 +75,28 @@ public class Revolut implements RevolutAPI {
     @Override
     public double getTotalAmount() {
         int totalAmount = 0;
-        for(Account account : accounts){
-            totalAmount += account.getAmount();
+        for (Account account : accounts) {
+            if(account.getCurrency().equals("BGN")) {
+                totalAmount += account.getAmount();
+            }
+            else{
+                totalAmount += account.getAmount() * EUR_TO_BGN_EXCHANGE_RATE;
+            }
         }
         return totalAmount;
     }
 
-    private boolean isValidAccount(Account account){
-        for(Account a : accounts){
-            if(a.equals(account)){
+    private boolean isBlockedURL(String URL) {
+        int indexOfBlockedDomain = URL.lastIndexOf(BLOCKED_TOP_LEVEL_DOMAIN);
+        if (indexOfBlockedDomain == URL.length() - 4) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isValidAccount(Account account) {
+        for (Account a : accounts) {
+            if (a.equals(account)) {
                 return true;
             }
         }
