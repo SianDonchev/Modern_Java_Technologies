aecb6051413811fe10671c3c4e135056d1b0e18b
unknown <siandonchev@gmail.com>
2021-03-07 01:13:47 +0200

    register,unregister and detDeviceById methods implementation added

--- Lab-4/src/bg/sofia/uni/fmi/mjt/smartcity/hub/SmartCityHub.java
+++ Lab-4/src/bg/sofia/uni/fmi/mjt/smartcity/hub/SmartCityHub.java
@@ -1,4 +1,93 @@
 package bg.sofia.uni.fmi.mjt.smartcity.hub;
 
+import bg.sofia.uni.fmi.mjt.smartcity.device.SmartDevice;
+import bg.sofia.uni.fmi.mjt.smartcity.enums.DeviceType;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
 public class SmartCityHub {
+    private LinkedHashMap<String,SmartDevice> smartDevices;
+
+    public SmartCityHub() {
+
+    }
+
+    /**
+     * Adds a @device to the SmartCityHub.
+     *
+     * @throws IllegalArgumentException         in case @device is null.
+     * @throws DeviceAlreadyRegisteredException in case the @device is already registered.
+     */
+    public void register(SmartDevice device) throws DeviceAlreadyRegisteredException {
+        if (smartDevices.containsKey(device.getId())) {
+            throw new UnsupportedOperationException();
+        }
+        smartDevices.put(device.getId(),device);
+    }
+
+    /**
+     * Removes the @device from the SmartCityHub.
+     *
+     * @throws IllegalArgumentException in case null is passed.
+     * @throws DeviceNotFoundException  in case the @device is not found.
+     */
+    public void unregister(SmartDevice device) throws DeviceNotFoundException {
+        if (!smartDevices.containsKey(device.getId())) {
+            throw new UnsupportedOperationException();
+        }
+        smartDevices.remove(device.getId());
+    }
+
+    /**
+     * Returns a SmartDevice with an ID @id.
+     *
+     * @throws IllegalArgumentException in case @id is null.
+     * @throws DeviceNotFoundException  in case device with ID @id is not found.
+     */
+    public SmartDevice getDeviceById(String id) throws DeviceNotFoundException {
+        if (!smartDevices.containsKey(id)) {
+            throw new UnsupportedOperationException();
+        }
+        return smartDevices.remove(id);
+    }
+
+    /**
+     * Returns the total number of devices with type @type registered in SmartCityHub.
+     *
+     * @throws IllegalArgumentException in case @type is null.
+     */
+    public int getDeviceQuantityPerType(DeviceType type) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns a collection of IDs of the top @n devices which consumed
+     * the most power from the time of their installation until now.
+     * <p>
+     * The total power consumption of a device is calculated by the hours elapsed
+     * between the two LocalDateTime-s: the installation time and the current time (now)
+     * multiplied by the stated nominal hourly power consumption of the device.
+     * <p>
+     * If @n exceeds the total number of devices, return all devices available sorted by the given criterion.
+     *
+     * @throws IllegalArgumentException in case @n is a negative number.
+     */
+    public Collection<String> getTopNDevicesByPowerConsumption(int n) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns a collection of the first @n registered devices, i.e the first @n that were added
+     * in the SmartCityHub (registration != installation).
+     * <p>
+     * If @n exceeds the total number of devices, return all devices available sorted by the given criterion.
+     *
+     * @throws IllegalArgumentException in case @n is a negative number.
+     */
+    public Collection<SmartDevice> getFirstNDevicesByRegistration(int n) {
+        throw new UnsupportedOperationException();
+    }
 }
+
