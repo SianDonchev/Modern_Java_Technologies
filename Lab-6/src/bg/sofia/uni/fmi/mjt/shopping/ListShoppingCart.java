eac4e29258b2e9f28c3a9cacd611fa7bebed87cd
unknown <siandonchev@gmail.com>
2021-04-03 23:42:56 +0300

    Styling refactoring in ListShoppingCart

--- Lab-6/src/bg/sofia/uni/fmi/mjt/shopping/ListShoppingCart.java
+++ Lab-6/src/bg/sofia/uni/fmi/mjt/shopping/ListShoppingCart.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class ListShoppingCart implements ShoppingCart {
-
     private ArrayList<Item> items;
     private ProductCatalog catalog;
 
